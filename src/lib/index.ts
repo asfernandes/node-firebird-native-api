7cf0b6ae7a8d6c364d0cb57758e97c804fee6572
sywka <lomashko.yuri@gmail.com>
2018-05-03 22:50:05 +0300

    use single quotes for strings

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,12 +6,12 @@ import * as os from 'os';
 /** Gets the default platform Firebird client library filename. */
 export function getDefaultLibraryFilename(): string {
 	switch (os.platform()) {
-		case "win32":
-			return "fbclient.dll";
-		case "darwin":
-			return "libfbclient.dylib";
+		case 'win32':
+			return 'fbclient.dll';
+		case 'darwin':
+			return 'libfbclient.dylib';
 		default:
-			return "libfbclient.so";
+			return 'libfbclient.so';
 	}
 }
 
